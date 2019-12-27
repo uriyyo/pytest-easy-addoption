8d91d6629ae619b00452a3b0c5baa9e89f11d2bd
uriyyo <1998uriyyo@gmail.com>
2019-12-27 10:59:47 +0200

    🎉 Add ability define addoptions as entry point

--- _pytest_easy_addoption/plugin.py
+++ _pytest_easy_addoption/plugin.py
@@ -2,10 +2,19 @@ from typing import TYPE_CHECKING
 
 from pytest import hookimpl
 
+from pkg_resources import iter_entry_points
+
 from .config_holder import ConfigHolder
 
 if TYPE_CHECKING:
     from _pytest.config import Config
+    from _pytest.config.argparsing import Parser
+
+
+def pytest_addoption(parser: "Parser") -> None:
+    for entry in iter_entry_points(group="pytest-easy-addoption"):
+        cls = entry.resolve()
+        cls.register(parser)
 
 
 @hookimpl(tryfirst=True)
